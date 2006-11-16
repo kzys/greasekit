ca1ef0cebe554ee648c4e06b1e7c332480c27f88
kzys <kzys@4ce0379f-c439-0410-9ddc-fd297e33974e>
2006-11-16 13:32:54 +0000

    Version 0.4.

---
+++ tags/RELEASE-0.4/WildcardPattern.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2006 KATO Kazuyoshi <kzys@8-p.info>
+ * This source code is released under the MIT license.
+ */
+
+#import <Cocoa/Cocoa.h>
+
+@interface WildcardPattern : NSObject {
+	NSString* pattern_;
+}
+
+- (id) initWithString: (NSString*) s;
+- (BOOL) isMatch: (NSString*) s;
+
+@end
