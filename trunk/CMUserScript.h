c42185419ad960b2727d4c2980413f8f219f9bcd
kzys <kzys@4ce0379f-c439-0410-9ddc-fd297e33974e>
2006-11-16 12:39:05 +0000

    Implement GUI.

--- trunk/CMUserScript.h
+++ trunk/CMUserScript.h
@@ -1,16 +1,37 @@
+/*
+ * Copyright (c) 2006 KATO Kazuyoshi <kzys@8-p.info>
+ * This source code is released under the MIT license.
+ */
+
 #import <Cocoa/Cocoa.h>
 
-
 @interface CMUserScript : NSObject {
+	NSString* name_;
+	NSString* description_;
 	NSString* script_;
+	
 	NSMutableArray* include_;
+	NSMutableArray* exclude_;
+	
+	NSString* basename_;
+	NSString* fullPath_;
 }
 
 + (NSDictionary*) parseMetadata: (NSString*) script;
 
+- (id) initWithString: (NSString*) script;
 - (id) initWithContentsOfFile: (NSString*) path;
+- (id) initWithContentsOfURL: (NSURL*) url;
 
+// Getter
+- (NSString*) name;
+- (NSString*) description;
 - (NSString*) script;
-- (BOOL) isIncluded: (NSURL*) url;
+
+- (BOOL) isInstalled: (NSString*) path;
+- (BOOL) install: (NSString*) path;
+- (BOOL) uninstall;
+
+- (BOOL) isMatched: (NSURL*) url;
 
 @end
--- trunk/CMUserScript.m
+++ trunk/CMUserScript.m
@@ -1,25 +1,75 @@
+/*
+ * Copyright (c) 2006 KATO Kazuyoshi <kzys@8-p.info>
+ * This source code is released under the MIT license.
+ */
+
 #import "CMUserScript.h"
 #import "WildcardPattern.h"
 
 @implementation CMUserScript
+- (BOOL) isInstalled: (NSString*) scriptDir
+{
+	NSString* path;
+	path = [NSString stringWithFormat: @"%@/%@", scriptDir, basename_];
+	
+	return [[NSFileManager defaultManager] fileExistsAtPath: path];
+}
+
+- (NSString*) name
+{
+	return name_;
+}
+
+- (NSString*) description
+{
+	return description_;
+}
 
 - (NSString*) script
 {
 	return script_;
 }
 
-- (BOOL) isIncluded: (NSURL*) url
+- (BOOL) install: (NSString*) path
+{
+	[fullPath_ release];
+	fullPath_ = [[NSString alloc] initWithFormat: @"%@/%@", path, basename_];
+	
+	return [script_ writeToFile: fullPath_ atomically: YES];
+}
+
+- (BOOL) uninstall
+{
+	if (fullPath_) {
+		return [[NSFileManager defaultManager] removeFileAtPath: fullPath_
+														handler: nil];
+	}
+	return NO;
+}
+
+- (BOOL) isMatched: (NSURL*) url
+		  patterns: (NSArray*) ary
 {
 	NSString* str = [url absoluteString];
 	
 	int i;
-	for (i = 0; i < [include_ count]; i++) {
-		WildcardPattern* pat = [include_ objectAtIndex: i];
+	for (i = 0; i < [ary count]; i++) {
+		WildcardPattern* pat = [ary objectAtIndex: i];
 		if ([pat isMatch: str]) {
 			return YES;
 		}
 	}
 	
+	return NO;
+}
+
+- (BOOL) isMatched: (NSURL*) url
+{
+	if ([self isMatched: url patterns: include_]) {
+		if (! [self isMatched: url patterns: exclude_]) {
+			return YES;
+		}
+	}
 	return NO;
 }
 
@@ -37,10 +87,14 @@
 							intoString: NULL];
 	
 	// Read key until whitespace
+	if ([scanner isAtEnd])
+		return;
 	[scanner scanUpToCharactersFromSet: [NSCharacterSet whitespaceCharacterSet]
 							intoString: &key];
 
 	// Read value until "\r" or "\n"
+	if ([scanner isAtEnd])
+		return;
 	[scanner scanUpToCharactersFromSet: [NSCharacterSet characterSetWithCharactersInString: @"\n\r"]
 							intoString: &value];
 	
@@ -75,26 +129,88 @@
 		}
 	}
 	
-	return [result autorelease];
+	if ([result count] == 0) {
+		[result release];
+		return nil;
+	} else {
+		return [result autorelease];
+	}
 }
 
-- (id) initWithContentsOfFile: (NSString*) path
++ (NSArray*) createPatterns: (NSArray*) ary
 {
-	self = [self init];
+	NSMutableArray* result = [[NSMutableArray alloc] init];
 	
-	script_ = [[NSString alloc] initWithContentsOfFile: path];
-	NSDictionary* metadata = [CMUserScript parseMetadata: script_];
-	
-	NSArray* ary = [metadata objectForKey: @"@include"];
 	int i;
 	for (i = 0; i < [ary count]; i++) {
 		WildcardPattern* pat;
 		pat = [[WildcardPattern alloc] initWithString: [ary objectAtIndex: i]];
-		[include_ addObject: pat];
+		[result addObject: pat];
 	}
 	
-	NSLog(@"regist: %@", 
-		  [[metadata objectForKey: @"@name"] objectAtIndex: 0]);
+	return [result autorelease];
+}
+
+- (id) initWithString: (NSString*) script
+{
+	NSDictionary* metadata = [CMUserScript parseMetadata: script];
+	if (! metadata)
+		return nil;
+	
+	if ([[metadata objectForKey: @"@name"] count] &&
+		[[metadata objectForKey: @"@description"] count] &&
+		[[metadata objectForKey: @"@include"] count])
+		;
+	else
+		return nil;
+
+	self = [self init];
+	
+	script_ = [script retain];
+	
+	// name, description
+	name_ = [[[metadata objectForKey: @"@name"] objectAtIndex: 0] retain];
+	description_ = [[[metadata objectForKey: @"@description"] objectAtIndex: 0] retain];
+	
+	// include
+	NSArray* ary;
+	ary = [CMUserScript createPatterns: [metadata objectForKey: @"@include"]];
+	[include_ addObjectsFromArray: ary];
+	
+	// exclude
+	ary = [CMUserScript createPatterns: [metadata objectForKey: @"@exclude"]];
+	[exclude_ addObjectsFromArray: ary];
+	
+	return self;
+}
+
+- (id) initWithContentsOfFile: (NSString*) path
+{
+	NSString* script = [[NSString alloc] initWithContentsOfFile: path];
+	
+	self = [self initWithString: script];
+	[script release];	
+
+	if (! self)
+		return nil;
+	
+	basename_ = [[path lastPathComponent] retain];
+	fullPath_ = [path retain];
+	
+	return self;
+}
+
+- (id) initWithContentsOfURL: (NSURL*) url
+{
+	NSString* script = [[NSString alloc] initWithContentsOfURL: url];
+	
+	self = [self initWithString: script];
+	[script release];
+	
+	if (! self)
+		return nil;
+	
+	basename_ = [[[url absoluteString] lastPathComponent] retain];
 	
 	return self;
 }
@@ -102,18 +218,35 @@
 #pragma mark Override
 - (id) init
 {
+	// NSLog(@"CMUserScript %p - init", self);
+
 	self = [super init];
 	
 	script_ = nil;
+	
+	name_ = nil;
+	description_ = nil;
 	include_ = [[NSMutableArray alloc] init];
+	exclude_ = [[NSMutableArray alloc] init];
+	
+	basename_ = nil;
+	fullPath_ = nil;
 	
 	return self;
 }
 
 - (void) dealloc
 {
+	// NSLog(@"CMUserScript %p - dealloc", self);
+
 	[script_ release];
+
+	[name_ release];
+	[description_ release];
 	[include_ release];
+	
+	[basename_ release];
+	[fullPath_ release];
 	
 	[super dealloc];
 }
