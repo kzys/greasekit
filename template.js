752b11ad79630e3ee6e075a744eb68af7f632ee9
kzys <kzys@4ce0379f-c439-0410-9ddc-fd297e33974e>
2007-06-16 15:54:19 +0000

    Add semicolon.

--- template.js
+++ template.js
@@ -3,14 +3,15 @@
     with ({ unsafeWindow: window, document: window.document, location: window.location, window: undefined }) {
         // define GM functions
         var GM_log = function (s) {
+            unsafeWindow.console.log('GM_log: ' + s);
             return <bridge>.gmLog_(s);
-        }
+        };
         var GM_getValue = function (k, d) {
             return <bridge>.gmValueForKey_defaultValue_scriptName_namespace_(k, d, "<name>", "<namespace>");
-        }
+        };
         var GM_setValue = function (k, v) {
             return <bridge>.gmSetValue_forKey_scriptName_namespace_(v, k, "<name>", "<namespace>");
-        }
+        };
         /* Not implemented yet
         var GM_registerMenuCommand = function (t, c) {
             <bridge>.gmRegisterMenuCommand_callback_(t, c);
@@ -18,7 +19,7 @@
         */
         var GM_xmlhttpRequest = function (d) {
             return <bridge>.gmXmlhttpRequest_(d);
-        }
+        };
 
         <body>;
     }
