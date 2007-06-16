1a030b991f7195cac20b9c1ca43195ad13ca6a23
kzys <kzys@4ce0379f-c439-0410-9ddc-fd297e33974e>
2007-06-16 15:16:40 +0000

    Use random name to bridge variable.

--- trunk/template.js
+++ trunk/template.js
@@ -1,17 +1,15 @@
-(function (bridge) {
+(function (<bridge>) {
     // unsafeWindow
-    var unsafeWindow = window;
-
-    with ({ document: unsafeWindow.document, location: unsafeWindow.location, window: {} }) {
+    with ({ unsafeWindow: window, document: window.document, location: window.location, window: undefined }) {
         // define GM functions
         var GM_log = function (s) {
-            return bridge.gmLog_(s);
+            return <bridge>.gmLog_(s);
         }
         var GM_getValue = function (k, d) {
-            return bridge.gmValueForKey_defaultValue_scriptName_namespace_(k, d, "<name>", "<namespace>");
+            return <bridge>.gmValueForKey_defaultValue_scriptName_namespace_(k, d, "<name>", "<namespace>");
         }
         var GM_setValue = function (k, v) {
-            return bridge.gmSetValue_forKey_scriptName_namespace_(v, k, "<name>", "<namespace>");
+            return <bridge>.gmSetValue_forKey_scriptName_namespace_(v, k, "<name>", "<namespace>");
         }
         /* Not implemented yet
         var GM_registerMenuCommand = function (t, c) {
@@ -19,7 +17,7 @@
         }
         */
         var GM_xmlhttpRequest = function (d) {
-            return bridge.gmXmlhttpRequest_(d);
+            return <bridge>.gmXmlhttpRequest_(d);
         }
 
         <body>;
