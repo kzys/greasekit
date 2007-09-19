8f2368c89a642b29f8c66e6b8af82b5293be9a8e
kzys <kzys@4ce0379f-c439-0410-9ddc-fd297e33974e>
2007-09-19 16:19:39 +0000

    reindent and delete trailing spaces.

--- trunk/CMController.m
+++ trunk/CMController.m
@@ -42,13 +42,13 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
 - (NSString*) loadScriptTemplate
 {
     NSBundle* bundle = [NSBundle bundleWithIdentifier: BUNDLE_IDENTIFIER];
-    NSString* path = [NSString stringWithFormat: @"%@/template.js", [bundle resourcePath]];   
+    NSString* path = [NSString stringWithFormat: @"%@/template.js", [bundle resourcePath]];
     return [NSString stringWithContentsOfFile: path];
 }
 
 - (NSArray*) scripts
 {
-	return scripts_;
+    return scripts_;
 }
 
 - (NSDictionary*) scriptsConfig
@@ -60,14 +60,14 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
 - (void) saveScriptsConfig
 {
     NSMutableDictionary* dict = [NSMutableDictionary dictionary];
-    
+
     NSEnumerator* enumerator = [scripts_ objectEnumerator];
     CMUserScript* script;
     while (script = [enumerator nextObject]) {
         [dict setObject: [NSNumber numberWithBool: [script isEnabled]]
                  forKey: [script basename]];
     }
-    
+
     NSString* path = [CONFIG_PATH stringByExpandingTildeInPath];
     [dict writeToFile: path atomically: YES];
 }
@@ -81,7 +81,7 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
 - (void) installScript: (CMUserScript*) s
 {
     [s install: scriptDir_];
-    
+
     [scripts_ addObject: s];
     [self saveScriptsConfig];
     [self reloadUserScripts: nil];
@@ -95,17 +95,17 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
     for (i = 0; i < count; i++) {
         [menu removeItemAtIndex: 0];
     }
-    
-    for (i = 0; i < [scripts_ count]; i++) {  
+
+    for (i = 0; i < [scripts_ count]; i++) {
         CMUserScript* script = [scripts_ objectAtIndex: i];
-        
+
         NSMenuItem* item = [[NSMenuItem alloc] init];
-        
+
         [item setTag: i];
         [item setTarget: self];
         [item setAction: @selector(toggleScriptEnable:)];
         [item setState: [script isEnabled] ? NSOnState : NSOffState];
-        
+
         [item setTitle: [script name]];
         [menu insertItem: item atIndex: i];
     }
@@ -113,117 +113,117 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
 
 - (void) loadUserScripts
 {
-	NSFileManager* manager;
-	manager = [NSFileManager defaultManager];
-	
-	NSArray* files;
-	files = [manager directoryContentsAtPath: scriptDir_];
-	
-	if (! files) {
-		[manager createDirectoryAtPath: scriptDir_
-							attributes: nil];
-		files = [manager directoryContentsAtPath: scriptDir_];
-	}
-	
+    NSFileManager* manager;
+    manager = [NSFileManager defaultManager];
+
+    NSArray* files;
+    files = [manager directoryContentsAtPath: scriptDir_];
+
+    if (! files) {
+        [manager createDirectoryAtPath: scriptDir_
+                            attributes: nil];
+        files = [manager directoryContentsAtPath: scriptDir_];
+    }
+
     NSDictionary* config = [self scriptsConfig];
-    
-	[self willChangeValueForKey: @"scripts"];
-	
-	int i;
-	for (i = 0; i < [files count]; i++) {
-		NSString* path;
-		path = [NSString stringWithFormat: @"%@/%@", scriptDir_, [files objectAtIndex: i]];
-		
-		if (! [path hasSuffix: @".user.js"]) {
-			continue;
-		}
-		
-		CMUserScript* script;
-		script = [[CMUserScript alloc] initWithContentsOfFile: path];
+
+    [self willChangeValueForKey: @"scripts"];
+
+    int i;
+    for (i = 0; i < [files count]; i++) {
+        NSString* path;
+        path = [NSString stringWithFormat: @"%@/%@", scriptDir_, [files objectAtIndex: i]];
+
+        if (! [path hasSuffix: @".user.js"]) {
+            continue;
+        }
+
+        CMUserScript* script;
+        script = [[CMUserScript alloc] initWithContentsOfFile: path];
 
         [script setEnabled: [[config objectForKey: [script basename]] intValue]];
-		
-		[scripts_ addObject: script];
-		[script release];
-	}
-	[self didChangeValueForKey: @"scripts"];
-    
+
+        [scripts_ addObject: script];
+        [script release];
+    }
+    [self didChangeValueForKey: @"scripts"];
+
     [self reloadMenu];
 }
 
 - (NSArray*) matchedScripts: (NSURL*) url
 {
-	NSMutableArray* result = [[NSMutableArray alloc] init];
-	
-	int i;
-	for (i = 0; i < [scripts_ count]; i++) {
-		CMUserScript* script = [scripts_ objectAtIndex: i];
-		if ([script isEnabled] && [script isMatched: url]) {
-			[result addObject: script];
-		}
-	}
-	
-	return [result autorelease];
+    NSMutableArray* result = [[NSMutableArray alloc] init];
+
+    int i;
+    for (i = 0; i < [scripts_ count]; i++) {
+        CMUserScript* script = [scripts_ objectAtIndex: i];
+        if ([script isEnabled] && [script isMatched: url]) {
+            [result addObject: script];
+        }
+    }
+
+    return [result autorelease];
 }
 
 - (void) installAlertDidEnd: (NSAlert*) alert
-				 returnCode: (int) returnCode
-				contextInfo: (void*) contextInfo
+                 returnCode: (int) returnCode
+                contextInfo: (void*) contextInfo
 {
-	CMUserScript* script = (CMUserScript*) contextInfo;
-	if (returnCode == NSAlertFirstButtonReturn) {
+    CMUserScript* script = (CMUserScript*) contextInfo;
+    if (returnCode == NSAlertFirstButtonReturn) {
         [self installScript: script];
-	}
-	[script release];
+    }
+    [script release];
 }
 
 - (void) showInstallAlertSheet: (CMUserScript*) script
-					   webView: (WebView*) webView
+                       webView: (WebView*) webView
 {
-	NSAlert* alert = [[NSAlert alloc] init];
+    NSAlert* alert = [[NSAlert alloc] init];
 
-	// Informative Text
-	NSMutableString* text = [[NSMutableString alloc] init];
-	
-	if ([script name] && [script description]) {
-		[text appendFormat: @"%@ - %@", [script name], [script description]];
-	} else {
-		if ([script name])
-			[text appendString: [script name]];
-		else if ([script description])
-			[text appendString: [script description]];
-	}
-	
-	[alert setInformativeText: text];
-	[text release];
-	
-	// Message and Buttons
-	if([script isInstalled: scriptDir_]) {
-		[alert setMessageText: @"This script is installed, Override?"];	
-		[alert addButtonWithTitle: @"Override"];
-	} else {
-		[alert setMessageText: @"Install this script?"];	
-		[alert addButtonWithTitle: @"Install"];
-	}
-	[alert addButtonWithTitle: @"Cancel"];
-	
-	// Begin Sheet
-	[alert beginSheetModalForWindow: [webView window]
-					  modalDelegate: self
-					 didEndSelector: @selector(installAlertDidEnd:returnCode:contextInfo:)
-						contextInfo: script];	
+    // Informative Text
+    NSMutableString* text = [[NSMutableString alloc] init];
+
+    if ([script name] && [script description]) {
+        [text appendFormat: @"%@ - %@", [script name], [script description]];
+    } else {
+        if ([script name])
+            [text appendString: [script name]];
+        else if ([script description])
+            [text appendString: [script description]];
+    }
+
+    [alert setInformativeText: text];
+    [text release];
+
+    // Message and Buttons
+    if([script isInstalled: scriptDir_]) {
+        [alert setMessageText: @"This script is installed, Override?"];
+        [alert addButtonWithTitle: @"Override"];
+    } else {
+        [alert setMessageText: @"Install this script?"];
+        [alert addButtonWithTitle: @"Install"];
+    }
+    [alert addButtonWithTitle: @"Cancel"];
+
+    // Begin Sheet
+    [alert beginSheetModalForWindow: [webView window]
+                      modalDelegate: self
+                     didEndSelector: @selector(installAlertDidEnd:returnCode:contextInfo:)
+                        contextInfo: script];
 }
 
 + (int) countNewDocument: (WebView*) webView
 {
     if (! [[webView mainFrame] DOMDocument]) {
-		return 0;
-	}
-    
+        return 0;
+    }
+
     // Eval!
     id scriptObject = [webView windowScriptObject];
     WebScriptObject* func = [scriptObject evaluateWebScript: @"(function(doc){if(!doc.body.__creammonkeyed__){doc.body.__creammonkeyed__=true;return true;}})"];
-    
+
     // Eval Once?
     NSArray* frames = [[webView mainFrame] childFrames];
     if ([frames count] > 0) {
@@ -309,10 +309,10 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
 {
     DEBUG_LOG(@"CMController %@ - progressStarted: %@", self, n);
     NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
-	[center addObserver: self
-			   selector: @selector(progressChanged:)
-				   name: WebViewProgressEstimateChangedNotification
-				 object: [n object]];
+    [center addObserver: self
+               selector: @selector(progressChanged:)
+                   name: WebViewProgressEstimateChangedNotification
+                 object: [n object]];
 }
 
 + (BOOL) isSelectorExcludedFromWebScript: (SEL) sel
@@ -382,9 +382,9 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
                       forKey: name];
     }
     [dict setObject: value forKey: key];
-    
+
     [self saveScriptValues];
-    
+
     return nil;
 }
 
@@ -437,74 +437,74 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
 - (IBAction) toggleScriptEnable: (id) sender
 {
     CMUserScript* script = [scripts_ objectAtIndex: [sender tag]];
-    
+
     [script setEnabled: [sender state] != NSOnState];
     [sender setState: [script isEnabled] ? NSOnState : NSOffState];
-    
+
     [self saveScriptsConfig];
 }
 
 - (IBAction) uninstallSelected: (id) sender
 {
-	CMUserScript* script = [[scriptsController selectedObjects] objectAtIndex: 0];
-	[script uninstall];
-	
-	[self reloadUserScripts: sender];
+    CMUserScript* script = [[scriptsController selectedObjects] objectAtIndex: 0];
+    [script uninstall];
+
+    [self reloadUserScripts: sender];
 }
 
 - (IBAction) orderFrontAboutPanel: (id) sender
 {
-	NSImage* icon = [[NSWorkspace sharedWorkspace] iconForFileType: @"bundle"];
-	[icon setSize: NSMakeSize(128, 128)];
-	
-	NSDictionary* options;
-	options = [NSDictionary dictionaryWithObjectsAndKeys: 
-		@"Creammonkey",  @"ApplicationName",
-		icon,  @"ApplicationIcon",
-		@"",  @"Version",
-		@"Version 1.1",  @"ApplicationVersion",
-		@"Copyright (c) 2006-2007 KATO Kazuyoshi",  @"Copyright",
-		nil];
-	[NSApp orderFrontStandardAboutPanelWithOptions: options];
+    NSImage* icon = [[NSWorkspace sharedWorkspace] iconForFileType: @"bundle"];
+    [icon setSize: NSMakeSize(128, 128)];
+
+    NSDictionary* options;
+    options = [NSDictionary dictionaryWithObjectsAndKeys:
+                                @"Creammonkey",  @"ApplicationName",
+                            icon,  @"ApplicationIcon",
+                            @"",  @"Version",
+                            @"Version 1.1",  @"ApplicationVersion",
+                            @"Copyright (c) 2006-2007 KATO Kazuyoshi",  @"Copyright",
+                            nil];
+    [NSApp orderFrontStandardAboutPanelWithOptions: options];
 }
 
 
 - (IBAction) reloadUserScripts: (id) sender
 {
-	[scripts_ release];
-	
-	scripts_ = [[NSMutableArray alloc] init];
-	[self loadUserScripts];
+    [scripts_ release];
+
+    scripts_ = [[NSMutableArray alloc] init];
+    [self loadUserScripts];
 }
 
 #pragma mark Override
 + (void) load
 {
-	[[self alloc] init];
+    [[self alloc] init];
 }
 
 - (id) init
 {
-	// Safari?
-	NSString* identifier = [[NSBundle mainBundle] bundleIdentifier];
-	if (! [identifier isEqual: @"com.apple.Safari"]) {
-		return nil;
-	}
-	
-	NSLog(@"CMController %p - init", self);
-	self = [super init];
-	if (! self)
-		return nil;
-	
-	scriptDir_ = [@"~/Library/Application Support/Creammonkey/" stringByExpandingTildeInPath];
-	[scriptDir_ retain];
-	
-	scripts_ = nil;    
-	
-	[NSBundle loadNibNamed: @"Menu.nib" owner: self];
+    // Safari?
+    NSString* identifier = [[NSBundle mainBundle] bundleIdentifier];
+    if (! [identifier isEqual: @"com.apple.Safari"]) {
+        return nil;
+    }
+
+    NSLog(@"CMController %p - init", self);
+    self = [super init];
+    if (! self)
+        return nil;
+
+    scriptDir_ = [@"~/Library/Application Support/Creammonkey/" stringByExpandingTildeInPath];
+    [scriptDir_ retain];
+
+    scripts_ = nil;
+
+    [NSBundle loadNibNamed: @"Menu.nib" owner: self];
 
     scriptTemplate_ = [[self loadScriptTemplate] retain];
-    
+
     NSString* path = [VALUES_PATH stringByExpandingTildeInPath];
     scriptValues_ = [NSDictionary dictionaryWithContentsOfFile: path];
     if (scriptValues_) {
@@ -512,44 +512,44 @@ static NSString* VALUES_PATH = @"~/Library/Application Support/Creammonkey/value
     } else {
         scriptValues_ = [[NSMutableDictionary alloc] init];
     }
-	
-	return self;
+
+    return self;
 }
 
 - (void) dealloc
 {
-	NSLog(@"CMController - dealloc");
-	
-	[scripts_ release];
-    
-	[super dealloc];
+    NSLog(@"CMController - dealloc");
+
+    [scripts_ release];
+
+    [super dealloc];
 }
 
 - (void) awakeFromNib
 {
-	[self reloadUserScripts: nil];
+    [self reloadUserScripts: nil];
 
-	// Menu
-	NSMenuItem* item;
+    // Menu
+    NSMenuItem* item;
 
-	item = [[NSMenuItem alloc] init];
-	[item setSubmenu: menu];
-	
-	[menu setTitle: @":)"];
-	
-	[[NSApp mainMenu] addItem: item];
-	[item release];
-    	
-	// Notification
-	NSNotificationCenter* center = [NSNotificationCenter defaultCenter];    
-	[center addObserver: self
-			   selector: @selector(progressStarted:)
-				   name: WebViewProgressStartedNotification
-				 object: nil];
-	[center addObserver: self
-			   selector: @selector(progressFinished:)
-				   name: WebViewProgressFinishedNotification
-				 object: nil];
+    item = [[NSMenuItem alloc] init];
+    [item setSubmenu: menu];
+
+    [menu setTitle: @":)"];
+
+    [[NSApp mainMenu] addItem: item];
+    [item release];
+
+    // Notification
+    NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+    [center addObserver: self
+               selector: @selector(progressStarted:)
+                   name: WebViewProgressStartedNotification
+                 object: nil];
+    [center addObserver: self
+               selector: @selector(progressFinished:)
+                   name: WebViewProgressFinishedNotification
+                 object: nil];
 }
 
 
