45ecb4ad0e326089bf7b012cb5736dda1c211c2e
kato.kazuyoshi <kato.kazuyoshi@4ce0379f-c439-0410-9ddc-fd297e33974e>
2007-10-11 07:40:23 +0000

    * removed createDefaultApplicationList.  * code cleanup.

--- GKLoader.m
+++ GKLoader.m
@@ -4,42 +4,36 @@
 static NSString* APPS_PATH = @"~/Library/Application Support/GreaseKit/apps.plist";
 
 @implementation GKLoader
-+ (NSArray*) createDefaultApplicationList
-{
-    NSString* path = [APPS_PATH stringByExpandingTildeInPath];
-
-    NSFileManager* manager;
-    manager = [NSFileManager defaultManager];
-    [manager createDirectoryAtPath: [path stringByDeletingLastPathComponent]
-                        attributes: nil];
-
-    return [NSArray arrayWithObject: @"com.apple.Safari"];
-}
 
 + (void) saveApplicationList: (NSArray*) apps
 {
     NSString* path = [APPS_PATH stringByExpandingTildeInPath];
-
-    NSFileManager* manager;
-    manager = [NSFileManager defaultManager];
-    [manager createDirectoryAtPath: [path stringByDeletingLastPathComponent]
-                        attributes: nil];
-
     [apps writeToFile: path atomically: YES];
 }
 
 + (void) load
 {
     NSString* path = [APPS_PATH stringByExpandingTildeInPath];
-    NSArray* apps = [NSArray arrayWithContentsOfFile: path];
-    if (! apps) {
-        apps = [self createDefaultApplicationList];
+
+    // create folder on "Application Support".
+    NSFileManager* manager;
+    manager = [NSFileManager defaultManager];
+    [manager createDirectoryAtPath: [path stringByDeletingLastPathComponent]
+                        attributes: nil];
+
+    // load application list
+    NSArray* apps = [[NSArray alloc] initWithContentsOfFile: path];
+    if (apps && [apps count] > 0) {
+        ;
+    } else {
+        apps = [[NSArray alloc] initWithObjects: @"com.apple.Safari", @"com.factorycity.DietPibb", nil];
     }
 
     NSString* identifier = [[NSBundle mainBundle] bundleIdentifier];
-
     if ([apps containsObject: identifier]) {
-        [[CMController alloc] init];
+        [[CMController alloc] initWithApplications: apps];
     }
+
+    [apps release];
 }
 @end
